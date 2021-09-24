06b0925fbe8a61af24b57e212cb97fe5d367c5ee
rohandogra <dogra.rohan25@gmail.com>
2021-09-25 01:51:26 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3030;
+const port = process.env.PORT || 3030;
+const path = require("path");
+
 const configure = require("./config/database");
 const router = require("./config/routes");
 
@@ -12,7 +14,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "coupon-code-validator-fe/build")));
+
 app.use("/v1", router);
+
+//* Handles any requests that don't match the ones above
+app.get("/*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname + "/coupon-code-validator-fe/build/index.html")
+  );
+});
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
